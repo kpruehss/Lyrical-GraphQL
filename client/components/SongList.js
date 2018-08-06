ad802c48e33f0201ae0c45892b5119df118f3b79
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-08-06 13:59:21 +1000

    Implement Routing and first query

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+import { SSL_OP_SINGLE_DH_USE } from 'constants';
 
 class SongList extends Component {
+  renderSongs() {
+    if (this.props.data.loading) {
+      return <div>loading...</div>;
+    }
+    return this.props.data.songs.map(song => {
+      return (
+        <li key={song.id} className="collection-item">
+          {song.title}
+        </li>
+      );
+    });
+  }
+
   render() {
-    return <div>Songlist</div>;
+    return <ul className="collection">{this.renderSongs()}</ul>;
   }
 }
 
 const query = gql`
   {
     songs {
+      id
       title
     }
   }
 `;
 
-export default SongList;
+export default graphql(query)(SongList);
